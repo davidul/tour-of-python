1db1fcca6423bd5703e1c141440d542508f199c9
David Ulicny <ulicny.david@gmail.com>
2024-05-07 12:13:06 +0200

    decorators

--- 05-decorators/decor.py
+++ 05-decorators/decor.py
@@ -3,18 +3,23 @@
 def hello():
     print("Hello")
 
+
 def decorator(func):
     def wrapper():
         print("Before")
         func()
         print("After")
+
     return wrapper
+
 
 @decorator
 def greet():
     print("Still here")
 
+
 greet()
+
 
 # decorator syntax
 # decorator with arguments
@@ -25,10 +30,13 @@ def my_decorator(func):
         print("Before")
         func(*args, **kwargs)
         print("After")
+
     return wrapper
+
 
 @my_decorator
 def greet2(name):
     print(f"Still here {name}")
+
 
 greet2("David")
